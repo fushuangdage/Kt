83e013a5d978a7d8757784ee6e5e01b44d11d6e9
fushuang <fushuangdage@163.com>
2018-03-28 17:46:31 +0800

    更新 匹配规则

--- app/src/main/java/com/example/fushuang/kt/view/activity/PassWordActivity.kt
+++ app/src/main/java/com/example/fushuang/kt/view/activity/PassWordActivity.kt
@@ -3,6 +3,7 @@ package com.example.fushuang.kt.view.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
+import android.widget.TextView
 import com.example.fushuang.kt.global.BaseSize
 import com.example.fushuang.kt.util.ToastUtil
 import com.example.fushuang.kt.view.view.PasswordStrengthView
@@ -37,7 +38,8 @@ class PassWordActivity : AppCompatActivity() {
         text = "校验"
         onClick {
           var password = password.text.toString()
-          checkPassword(password)
+          var message=checkPassword(password)
+          toast(message)
 
         }
       }
@@ -50,30 +52,30 @@ class PassWordActivity : AppCompatActivity() {
     }.let { setContentView(it) }
   }
 
-  private fun checkPassword(password: String) {
+  private fun checkPassword(password: String): String {
 
     var result = 60
+    var message = ""
     var passwordArray = password.toCharArray()
     when {
       password.contains(" ")
-      -> ToastUtil.toast("含有不合理字符")
+      -> message = "含有不合理字符"
 
       password.length < 6
-      -> ToastUtil.toast("密码长度必须大于5位")
+      -> message = "密码长度必须大于5位"
 
       Regex("[0-9]+").matches(password)
-      -> ToastUtil.toast("不能为纯数字")
+      -> message = "不能为纯数字"
 
       Regex("[a-z]+", RegexOption.IGNORE_CASE).matches(password)
-      -> ToastUtil.toast("不能为纯字母")
+      -> message = "不能为纯字母"
 
       Regex("[!@#$%¥^&*()_=+?]").containsMatchIn(password)
               && Regex("[a-z]").containsMatchIn(password)
               && Regex("[A-Z]").containsMatchIn(password)
       -> result = 100
 
-      Regex("[0-9]").containsMatchIn(password)
-              && Regex("[a-z]").containsMatchIn(password)
+      Regex("[a-z]").containsMatchIn(password)
               && Regex("[A-Z]").containsMatchIn(password)
       -> result = 80
 
@@ -82,22 +84,31 @@ class PassWordActivity : AppCompatActivity() {
     /**
      * 递增连续字符减分
      */
-    for (i in 0..passwordArray.size - 2)
-      if (passwordArray[i] - passwordArray[i + 1] == -1)
+    for (i in 0..passwordArray.size - 2) {
+      if (passwordArray[i] - passwordArray[i + 1] == -1) {
         repeatCount++
-    if (repeatCount >= 3) {
-      result -= 20
+        if (repeatCount >= 3) {
+          result -= 20
+          break
+        }
+      } else
+        repeatCount = 0
     }
     /**
      * 连续相同字符减分
      */
     repeatCount = 0
-    for (i in 0..passwordArray.size - 2)
-      if (passwordArray[i] - passwordArray[i + 1] == 0)
+    for (i in 0..passwordArray.size - 2) {
+      if (passwordArray[i] - passwordArray[i + 1] == 0) {
         repeatCount++
-    if (repeatCount >= 3) {
-      result -= 20
+        if (repeatCount >= 3) {
+          result -= 20
+          break
+        }
+      } else
+        repeatCount = 0
     }
+
     /**
      * 包含疑似生日减分减分
      */
@@ -112,12 +123,12 @@ class PassWordActivity : AppCompatActivity() {
     }
 
     when (result) {
-      in 90..200->ToastUtil.toast("强")
-      in 60..80->ToastUtil.toast("中")
-      in 0..60->ToastUtil.toast("弱")
+      in 90..200 -> message = "强"
+      in 60..80 -> message = "中"
+      in 0..60 -> message = "弱"
     }
 
-
+    return message
   }
 
 
